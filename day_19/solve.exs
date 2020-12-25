8a1d11267e63612c93bb83c9ce48769d6e3d14cb
Djordje Lacmanovic <djordje.lacmanovic@gmail.com>
2020-12-25 02:25:07 +0100

    day 19 p2

--- day_19/solve.exs
+++ day_19/solve.exs
@@ -1,15 +1,27 @@
 defmodule Solution do
   def part1() do
-    {rules, images} = read()
+    solve()
+  end
+
+  def part2() do
+    solve(fn
+      "8:" <> _ -> "8: 42 | 42 8"
+      "11:" <> _ -> "11: 42 31 | 42 11 31"
+      s -> s
+    end)
+  end
+
+  defp solve(line_modifier \\ & &1) do
+    {rules, images} = read(line_modifier)
     rule0 = build_rule(rules, 0) |> compile_rule()
     images |> Enum.count(&Regex.match?(rule0, &1))
   end
 
-  defp read() do
+  defp read(mod) do
     [rules, _, images] =
-      System.argv()
-      |> File.stream!()
+      File.stream!(System.argv())
       |> Enum.map(&String.trim/1)
+      |> Enum.map(&mod.(&1))
       |> Enum.chunk_by(&empty/1)
 
     {rules |> Enum.map(&parse_rule/1) |> Enum.into(%{}), images}
@@ -35,12 +47,14 @@ defmodule Solution do
         char |> String.replace("\"", "")
 
       complex ->
+        loop_handled = transform_loop(complex, rule_no)
+
         "(" <>
-          (Regex.scan(~r/\d+/, complex)
+          (Regex.scan(~r/\d+/, loop_handled)
            |> Enum.flat_map(& &1)
            |> Enum.uniq()
            |> Enum.reduce(
-             complex,
+             loop_handled,
              &String.replace(
                &2,
                Regex.compile!("(?<!\\d)#{&1}(?!\\d)"),
@@ -51,8 +65,21 @@ defmodule Solution do
     end
   end
 
+  defp transform_loop(str, rule_no) do
+    if Regex.match?(Regex.compile!("(?<!\\d)#{rule_no}(?!\\d)"), str) do
+      "(?'rec#{num_str(rule_no)}'#{str})"
+      |> String.replace(to_string(rule_no), "(?&rec#{num_str(rule_no)})*")
+    else
+      str
+    end
+  end
+
+  defp num_str(8), do: "eight"
+  defp num_str(11), do: "eleven"
+
   defp empty(""), do: true
   defp empty(_str), do: false
 end
 
-Solution.part1() |> IO.inspect()
+IO.puts("First part: #{Solution.part1()}")
+IO.puts("Second part: #{Solution.part2()}")
